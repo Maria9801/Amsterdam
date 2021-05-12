b600d681a62e5314e1fcd5f784dcff41a232dfcb
Maria9801 <47497779+Maria9801@users.noreply.github.com>
2021-05-12 17:52:12 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function toggle_visibility(title1){
+    let e = document.getElementById(title1);
+    if(e.style.display == "block")
+     e.style.display = "none";
+     else
+     e.style.display = "block";
+}
+alert('sd');
---
+++ js/menu_2.js
@@ -0,0 +1,9 @@
+ $(function () {
+    $('.open_meenu').click(function () {
+      $('.menu').toggleClass('menu_showw')
+
+    })
+ });
+function pr{
+	document.getElementById("about").append("dsd")
+}
