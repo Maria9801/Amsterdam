7bde864d67febdcd27463dde6928519562a7fea9
Maria9801 <47497779+Maria9801@users.noreply.github.com>
2021-05-12 17:51:05 +0300

    Delete js directory

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function toggle_visibility(title1){
-    let e = document.getElementById(title1);
-    if(e.style.display == "block")
-     e.style.display = "none";
-     else
-     e.style.display = "block";
-}
-alert('sd');
