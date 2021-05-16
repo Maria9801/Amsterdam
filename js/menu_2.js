56795d7cd4ca05eb3b2ae3829d345150d6fd4faa
Maria9801 <47497779+Maria9801@users.noreply.github.com>
2021-05-16 12:18:27 +0300

    Delete js directory

--- js/menu_2.js
+++
@@ -1,9 +0,0 @@
- $(function () {
-    $('.open_meenu').click(function () {
-      $('.menu').toggleClass('menu_showw')
-
-    })
- });
-function pr{
-	document.getElementById("about").append("dsd")
-}
